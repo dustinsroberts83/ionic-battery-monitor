9df1d16554e9e08c3e3317740a0321f926353f0f
Dustin <dustinroberts@outlook.com>
2025-08-25 10:18:09 -0500

    Add files via upload

---
+++ types/bluetooth.d.ts
@@ -0,0 +1,117 @@
+// Web Bluetooth API Type Definitions
+interface Navigator {
+  bluetooth: Bluetooth;
+}
+
+interface Bluetooth {
+  requestDevice(options: RequestDeviceOptions): Promise<BluetoothDevice>;
+  getAvailability(): Promise<boolean>;
+}
+
+interface RequestDeviceOptions {
+  filters?: BluetoothLEScanFilter[];
+  optionalServices?: BluetoothServiceUUID[];
+  acceptAllDevices?: boolean;
+}
+
+interface BluetoothLEScanFilter {
+  services?: BluetoothServiceUUID[];
+  name?: string;
+  namePrefix?: string;
+  manufacturerData?: BluetoothManufacturerDataFilter[];
+  serviceData?: BluetoothServiceDataFilter[];
+}
+
+interface BluetoothManufacturerDataFilter {
+  companyIdentifier: number;
+  dataPrefix?: BufferSource;
+  mask?: BufferSource;
+}
+
+interface BluetoothServiceDataFilter {
+  service: BluetoothServiceUUID;
+  dataPrefix?: BufferSource;
+  mask?: BufferSource;
+}
+
+type BluetoothServiceUUID = number | string;
+
+interface BluetoothDevice extends EventTarget {
+  id: string;
+  name?: string;
+  gatt?: BluetoothRemoteGATTServer;
+  watchAdvertisements(): Promise<void>;
+  unwatchAdvertisements(): void;
+  addEventListener(type: 'gattserverdisconnected', listener: (event: Event) => void): void;
+  addEventListener(type: 'advertisementreceived', listener: (event: BluetoothAdvertisingEvent) => void): void;
+}
+
+interface BluetoothRemoteGATTServer {
+  device: BluetoothDevice;
+  connected: boolean;
+  connect(): Promise<BluetoothRemoteGATTServer>;
+  disconnect(): void;
+  getPrimaryService(service: BluetoothServiceUUID): Promise<BluetoothRemoteGATTService>;
+  getPrimaryServices(service?: BluetoothServiceUUID): Promise<BluetoothRemoteGATTService[]>;
+}
+
+interface BluetoothRemoteGATTService extends EventTarget {
+  device: BluetoothDevice;
+  uuid: string;
+  isPrimary: boolean;
+  getCharacteristic(characteristic: BluetoothCharacteristicUUID): Promise<BluetoothRemoteGATTCharacteristic>;
+  getCharacteristics(characteristic?: BluetoothCharacteristicUUID): Promise<BluetoothRemoteGATTCharacteristic[]>;
+  getIncludedService(service: BluetoothServiceUUID): Promise<BluetoothRemoteGATTService>;
+  getIncludedServices(service?: BluetoothServiceUUID): Promise<BluetoothRemoteGATTService[]>;
+}
+
+type BluetoothCharacteristicUUID = number | string;
+
+interface BluetoothRemoteGATTCharacteristic extends EventTarget {
+  service: BluetoothRemoteGATTService;
+  uuid: string;
+  properties: BluetoothCharacteristicProperties;
+  value?: DataView;
+  getDescriptor(descriptor: BluetoothDescriptorUUID): Promise<BluetoothRemoteGATTDescriptor>;
+  getDescriptors(descriptor?: BluetoothDescriptorUUID): Promise<BluetoothRemoteGATTDescriptor[]>;
+  readValue(): Promise<DataView>;
+  writeValue(value: BufferSource): Promise<void>;
+  writeValueWithResponse(value: BufferSource): Promise<void>;
+  writeValueWithoutResponse(value: BufferSource): Promise<void>;
+  startNotifications(): Promise<BluetoothRemoteGATTCharacteristic>;
+  stopNotifications(): Promise<BluetoothRemoteGATTCharacteristic>;
+  addEventListener(type: 'characteristicvaluechanged', listener: (event: Event) => void): void;
+}
+
+interface BluetoothCharacteristicProperties {
+  broadcast: boolean;
+  read: boolean;
+  writeWithoutResponse: boolean;
+  write: boolean;
+  notify: boolean;
+  indicate: boolean;
+  authenticatedSignedWrites: boolean;
+  reliableWrite: boolean;
+  writableAuxiliaries: boolean;
+}
+
+type BluetoothDescriptorUUID = number | string;
+
+interface BluetoothRemoteGATTDescriptor {
+  characteristic: BluetoothRemoteGATTCharacteristic;
+  uuid: string;
+  value?: DataView;
+  readValue(): Promise<DataView>;
+  writeValue(value: BufferSource): Promise<void>;
+}
+
+interface BluetoothAdvertisingEvent extends Event {
+  device: BluetoothDevice;
+  uuids: string[];
+  manufacturerData: Map<number, DataView>;
+  serviceData: Map<string, DataView>;
+  name?: string;
+  appearance?: number;
+  rssi?: number;
+  txPower?: number;
+}
